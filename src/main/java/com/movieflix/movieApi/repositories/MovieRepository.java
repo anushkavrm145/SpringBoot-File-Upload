fafc1b4fbb949950c3a8994c9bee2d659458bbe5
anushka <anushkavrm145@gmail.com>
2024-09-18 12:08:58 +0530

    how to upload a file in backend.

---
+++ src/main/java/com/movieflix/movieApi/repositories/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.movieflix.movieApi.repositories;
+
+import com.movieflix.movieApi.entities.Movie;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+}
